8239e8355d048404bbe75c44909bbee719bef8f6
Andrey Opalev <andrey.o.opalev@yandex.ru>
2022-11-06 18:24:00 +0200

    Modifed supersaturation.py to be as API to all function

--- src/supersaturation.py
+++ src/supersaturation.py
@@ -1,108 +1,77 @@
-import matplotlib.pyplot as pyplot
+from experiments import ExperimentNotes
+from air_data import AirData
+from supersaturation_calc import SupersaturationCalc
 import matplotlib.dates as mdates
-import matplotlib.ticker as mticker
-import numpy as np
+import matplotlib.units as munits
+import matplotlib.dates as md
 import pandas as pd
-import datetime
-import vapor_pressure_over_water as vapor
+import os
+import datetime as datetime
+import numpy as np
+import matplotlib.pyplot as pyplot
+
 
 class Supersaturation:
-    """ Class to keep results """
 
-    def __init__(self):
-       "Does nothing yet"
+    def __init__(self,
+                 path_to_experiments,
+                 path_to_air_data,
+                 path_to_output):
+        self.experiments = ExperimentNotes(path_to_experiments)
+        self.air_data = AirData(path_to_air_data)
+        self.path_to_output = path_to_output
 
-    def calc_supersaturation(self, humidity, air_pressure, sample_pressure):
-        return (humidity / 100.0) * air_pressure * (1.0 / sample_pressure)
+    def print_experiments(self):
+        all_notes = self.experiments.get_experiment_list()
+        print(all_notes.to_string())
 
-    def compute(self,
-                start_time : datetime.time, 
-                end_time : datetime.time,
-                target_sample_temp : float,
-                estimated_supersat : float,
-                air_df : pd.DataFrame,
-                cooling_speed : float):
-    
-        # 1. Do a slice of data in interval [start_time, end_time]
-        cropped_air_data = air_df.loc[(air_df.Time >= start_time) & (air_df.Time < end_time)]
-        cropped_air_data = cropped_air_data.reset_index(drop=True)
+    def print_air_data_files(self):
+        filenames = self.air_data.get_filenames()
+        for f in filenames:
+            print(f)
 
-        # 2. Added a column "SampleTemperature" filled with target_sample_temp
-        cropped_air_data.loc[:, 'SampleTemp'] = [target_sample_temp] * len(cropped_air_data.index)
-        #     sampleTemperature = pd.DataFrame([target_sample_temp] * len(cropped_air_data.index), columns=['SampleTemp'])
-        #     cropped_air_data = pd.concat([cropped_air_data, sampleTemperature])
+    def compute_saturation(self, experiment_index):
+        # Get the experiment note
+        experiment = self.experiments.get_data(experiment_index)
+        print(experiment)
 
-        # 3. Modify SampleTemp considering cooling speed
-        dew_temperature_at_start_time = cropped_air_data.DewTemp[cropped_air_data.Time > start_time].iloc[0]
-        print("Dew temperature at the start time, deg: ", dew_temperature_at_start_time)
+        # Load air data correspondig to the date
+        date_str = experiment.date.strftime("%d%m%y")
+        self.air_data.load_data_file(date_str,
+                                     sep=";",
+                                     dateformat='%m.%d.%Y')
 
-        cropped_air_data.loc[0, 'SampleTemp'] = dew_temperature_at_start_time
+        # print("Type of time:", type(experiment.start.values[0]))
+        # Convert Pandas Series type to
+
+        # Compute saturation
+        saturation_calc = SupersaturationCalc()
+        saturation_calc.compute(experiment.start,          # start time
+                                experiment.end,            # end time
+                                experiment.ts,             # target sample temperature
+                                experiment.S,              # computed saturation?
+                                self.air_data.get_data(),  # Air data
+                                5.0)                       # Cooling speed, deg / min
+
+        # Save
+        output_path = self.save_to_csv(saturation_calc)
+
+        return output_path
         
-        for i in range(1, len(cropped_air_data.index)):
-        
-            dt = cropped_air_data['Time'].iloc[i] - cropped_air_data.Time.iloc[i - 1]
-            
-            print('Time interval elapsed, s:',
-                  dt.total_seconds())
-            
-            print('Previous sample temperature, deg.:',
-                  cropped_air_data.loc[i-1, 'SampleTemp'])
-            
-            print("Sample temperature coolled down to, deg: ",
-                  cropped_air_data['SampleTemp'].iloc[i-1] - (cooling_speed / 60.0) * dt.total_seconds())
+    def save_to_csv(self, supersaturation_calc):
 
-            cropped_air_data.loc[i, 'SampleTemp'] = cropped_air_data['SampleTemp'].iloc[i-1] - (cooling_speed / 60.0) * dt.total_seconds()
+        # Create folder if does not exist
+        if not os.path.exists(self.path_to_output):
+           os.makedirs(self.path_to_output)
 
-            if cropped_air_data['SampleTemp'].iloc[i] <= target_sample_temp:
-                cropped_air_data.loc[i, 'SampleTemp'] = target_sample_temp
-                break
-        
-            cropped_air_data.loc[cropped_air_data['SampleTemp'].isnull(), 'SampleTemp'] = target_sample_temp
-
-            supersaturation = self.calc_supersaturation(cropped_air_data['Humidity'], 
-                                                        vapor.get_vapor_pressure_interpolated(cropped_air_data['Temp']),
-                                                        vapor.get_vapor_pressure_interpolated(cropped_air_data['SampleTemp']))
-    
-            # 4. Add "Supersaturation"to the final data
-            # TODO: Create a new DataFrame by concat.
-            cropped_air_data.loc[:, 'Supersaturation'] = supersaturation
-    
-            # TODO: Move it out.
-            # 5. Made relative time and put it as string
-            # cropped_air_data.Time = cropped_air_data.Time - cropped_air_data.Time[0]
-            # cropped_air_data.Time = cropped_air_data.Time.dt.strftime("%H:%M:%S")
-            self.computed_supersaturation = cropped_air_data
-
-    def get_result(self):
-        return self.computed_supersaturation
-
-    def plot(self, title=""):
-        time = self.computed_supersaturation.Time
-        supersaturation = self.computed_supersaturation.Supersaturation
-    
-        time_deltas = [(t - time.loc[0]).total_seconds() for t in time]    
-    
-        fig, axes = pyplot.subplots(constrained_layout=True)
-
-        # Start time since 0
-    
-        axes.xaxis.set_major_locator(mticker.MaxNLocator(nbins = 6))
-        # axes.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
-        axes.set_xlabel('Time, s')
-        axes.set_ylabel('Supersaturation')
-        axes.plot(time_deltas, supersaturation)
-        axes.set_title(title)
-        pyplot.show()
-
-    def save_to_csv(self, path):
         # Make the printed date and time values without extra info
         # and don't change the computed data itself.
-        data = self.computed_supersaturation.copy()
-        data["Date"] = pd.to_datetime(data['Date'], format= '%d/%m/%Y').dt.date
+        data = supersaturation_calc.get_results().copy()
+        data["Date"] = pd.to_datetime(data['Date'], format='%d/%m/%Y').dt.date
 
         # Create a filename
         full_path = "{}/supersaturation_{}_{}_{}.csv".format(
-            path,
+            self.path_to_output,
             data["Date"].iloc[0],
             data["Time"].iloc[0].strftime('%H:%M:%S'),
             data["Time"].iloc[-1].strftime('%H:%M:%S'))
@@ -111,7 +80,66 @@ class Supersaturation:
         # the start and end time is used to generate filename
         data["Time"] = (data["Time"] - data["Time"].iloc[0])
         data["Time"] = pd.to_datetime(data['Time'].astype(str), format='0 days %H:%M:%S').dt.time
-                                      
-        data.to_csv(full_path)
-        
+
+        data.to_csv(full_path, sep=';')
+
         print("The computed results are saved in the {}.".format(full_path))
+
+        return full_path
+
+    def plot_results(self, output_file):
+        df = pd.read_csv(output_file,
+                         sep=';',
+                         encoding='cp1251',
+                         index_col=0,
+                         header=0,
+                         names=['Date', 'Time', 'Humidity', 'Temp', 'DewTemp', 'PSYC', 'SampleTemp', 'Supersaturation'])
+
+        t = pd.to_datetime(df['Date'] + ' ' + df['Time'],
+                           format="%Y-%m-%d %H:%M:%S")
+
+        # Dateformatter
+        t_fmt = md.DateFormatter('%H:%M:%S')
+    
+        # Time is always a problem
+        converter = mdates.ConciseDateConverter()
+        munits.registry[np.datetime64] = converter
+        munits.registry[datetime.date] = converter
+        munits.registry[datetime.datetime] = converter
+
+        fig = pyplot.figure(figsize=(10, 10))
+
+        # Plot Humidity
+        ax1 = fig.add_subplot(411)
+        ax1.set_ylabel('Humidity, %')
+        ax1.grid(True)
+        ax1.plot(t, df.Humidity, color='blue')
+
+        # Plot Temperature
+        ax2 = fig.add_subplot(412, sharex=ax1)
+        ax2.set_ylabel('Temperature, deg')
+        ax2.grid(True)
+        ax2.plot(t, df.Temp, color='orange')
+
+        # Plot Dew temperature
+        ax3 = fig.add_subplot(413, sharex=ax1, sharey=ax2)
+        ax3.set_ylabel('Dew temperature, deg')
+        ax3.grid(True)
+        ax3.plot(t, df.DewTemp, color='orange')
+
+        # Plot Supersaturation
+        ax4 = fig.add_subplot(414)
+        ax4.set_ylabel('Supersaturation')
+        ax4.grid(True)
+        ax4.plot(t, df.Supersaturation, color='red')
+
+        # Configure X axis
+        ax4.set_xlabel("Time")
+        ax4.xaxis.set_major_formatter(t_fmt)
+        
+        # Plot
+        ax1.set_title("Output data")
+        pyplot.setp(ax1.get_xticklabels(), visible=False)
+        pyplot.setp(ax2.get_xticklabels(), visible=False)
+        pyplot.setp(ax3.get_xticklabels(), visible=False)
+        pyplot.show()
